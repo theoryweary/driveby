b70dec411012916a050c99a4cab3d3ca7c19394d
brendon.kellner <brendon.kellner@cambridgesemantics.com>
2018-06-09 16:43:51 -0400

    Tried to upload to apple App storage but got some stupid error

--- ios/driveby/AppDelegate.m
+++ ios/driveby/AppDelegate.m
@@ -24,7 +24,8 @@
   NSLog(@"NSLOG ############################");
 //  NSLog(url);
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  //[[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"driveby"
